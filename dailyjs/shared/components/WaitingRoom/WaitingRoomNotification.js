d4636a9450d59a56810d238d276846b71c3ae27c
Jon <jon@daily.co>
2021-06-14 16:08:14 +0100

    added notification styles

--- dailyjs/shared/components/WaitingRoom/WaitingRoomNotification.js
+++ dailyjs/shared/components/WaitingRoom/WaitingRoomNotification.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { useCallState } from '../../contexts/CallProvider';
 import { useWaitingRoom } from '../../contexts/WaitingRoomProvider';
 import { Button } from '../Button';
+import { Card, CardBody, CardFooter } from '../Card';
 
 export const WaitingRoomNotification = () => {
   const { callObject } = useCallState();
@@ -57,35 +58,43 @@ export const WaitingRoomNotification = () => {
   const handleDenyClick = () => {
     denyAccess(hasMultiplePeopleWaiting ? 'all' : waitingParticipants[0].id);
   };
-  const handleClose = () => setShowNotification(false);
+  // const handleClose = () => setShowNotification(false);
 
   return (
-    <div className="waiting-room-notification">
-      <>
+    <Card className="waiting-room-notification">
+      <CardBody>
+        {hasMultiplePeopleWaiting
+          ? `${waitingParticipants.length} people would like to join the call`
+          : `${waitingParticipants[0].name} would like to join the call`}
+      </CardBody>
+      <CardFooter>
         {hasMultiplePeopleWaiting ? (
-          <Button onClick={handleViewAllClick}>View all</Button>
+          <Button onClick={handleViewAllClick} size="small" variant="success">
+            View all
+          </Button>
         ) : (
-          <Button onClick={handleAllowClick}>Allow</Button>
+          <Button onClick={handleAllowClick} size="small" variant="success">
+            Allow
+          </Button>
         )}
-        <Button onClick={handleDenyClick} variant="secondary">
-          {hasMultiplePeopleWaiting
-            ? 'waitingRoom.denyAll'
-            : 'waitingRoom.deny'}
+        <Button onClick={handleDenyClick} size="small" variant="error">
+          {hasMultiplePeopleWaiting ? 'Deny All' : 'Deny'}
         </Button>
-        <Button onClick={handleClose} variant="ghost">
-          Close
-        </Button>
-      </>
-
+      </CardFooter>
       <style jsx>{`
-        .waiting-room-notification {
+        :global(.waiting-room-notification) {
           position: absolute;
           right: var(--spacing-sm);
           top: var(--spacing-sm);
-          background: red;
+          z-index: 999;
+          box-shadow: var(--shadow-depth-2);
+        }
+        :global(.waiting-room-notification .card-footer) {
+          display: flex;
+          column-gap: var(--spacing-xxs);
         }
       `}</style>
-    </div>
+    </Card>
   );
 };
 
