d11dd71e2239a9b8fa15b2d3bebe1b5580592a09
Miguel Tineo <miketineo@gmail.com>
2021-08-26 22:46:15 +0200

    Reordering imports

--- dailyjs/live-streaming/components/App/App.js
+++ dailyjs/live-streaming/components/App/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
+import App from '@dailyjs/basic-call/components/App';
 import Amplify from 'aws-amplify';
 import awsconfig from '../../../../src/aws-exports';
+import { LiveStreamingProvider } from '../../contexts/LiveStreamingProvider';
 
 Amplify.configure(awsconfig);
-
-import App from '@dailyjs/basic-call/components/App';
-import { LiveStreamingProvider } from '../../contexts/LiveStreamingProvider';
 
 // Extend our basic call app component with the live streaming context
 export const AppWithLiveStreaming = () => (
