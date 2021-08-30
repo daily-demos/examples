b4a04e3d63fe60f03cc01cf4fc18f1105483274d
Miguel Tineo <miketineo@gmail.com>
2021-08-30 16:22:47 +0200

    API config

--- dailyjs/basic-call/pages/index.js
+++ dailyjs/basic-call/pages/index.js
@@ -10,8 +10,29 @@ import PropTypes from 'prop-types';
 import App from '../components/App';
 import { CreatingRoom } from '../components/CreatingRoom';
 import { Intro, NotConfigured } from '../components/Intro';
-import { API } from 'aws-amplify';
+import Amplify, { API } from 'aws-amplify';
 
+Amplify.configure({
+  // OPTIONAL - if your API requires authentication 
+  // Auth: {
+  //     // REQUIRED - Amazon Cognito Identity Pool ID
+  //     identityPoolId: 'XX-XXXX-X:XXXXXXXX-XXXX-1234-abcd-1234567890ab',
+  //     // REQUIRED - Amazon Cognito Region
+  //     region: 'XX-XXXX-X', 
+  //     // OPTIONAL - Amazon Cognito User Pool ID
+  //     userPoolId: 'XX-XXXX-X_abcd1234', 
+  //     // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
+  //     userPoolWebClientId: 'a1b2c3d4e5f6g7h8i9j0k1l2m3',
+  // },
+  API: {
+      endpoints: [
+          {
+              name: `${process.env.API_NAME}`,
+              endpoint: `${process.env.API_ENDPOINT}`
+          }
+      ]
+  }
+});
 /**
  * Index page
  * ---
--- dailyjs/live-streaming/components/App/App.js
+++ dailyjs/live-streaming/components/App/App.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import App from '@dailyjs/basic-call/components/App';
-import Amplify from 'aws-amplify';
 import { LiveStreamingProvider } from '../../contexts/LiveStreamingProvider';
-import awsconfig from '../../src/aws-exports';
-
-Amplify.configure(awsconfig);
 
 // Extend our basic call app component with the live streaming context
 export const AppWithLiveStreaming = () => (
