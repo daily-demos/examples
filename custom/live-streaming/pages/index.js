c889c43b50248d13ac2ae795415b2da1bd847fe1
J Taylor <jon@momentic.co>
2021-10-05 11:26:38 +0300

    initial commit

---
+++ custom/live-streaming/pages/index.js
@@ -0,0 +1,16 @@
+import Index from '@custom/basic-call/pages';
+import getDemoProps from '@custom/shared/lib/demoProps';
+
+export async function getStaticProps() {
+  const defaultProps = getDemoProps();
+
+  return {
+    props: {
+      ...defaultProps,
+      forceFetchToken: true,
+      forceOwner: true,
+    },
+  };
+}
+
+export default Index;
