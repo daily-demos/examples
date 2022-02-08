c683302ce33aa864bf90d85eee1a1f4698a3b06d
harshithpabbati <pabbatiharshith@gmail.com>
2022-02-08 14:43:21 +0530

    Use form elements and add submit handlers in chat, haircheck and the prejoin screen

--- custom/text-chat/components/ChatAside.js
+++ custom/text-chat/components/ChatAside.js
@@ -57,25 +57,28 @@ export const ChatAside = () => {
           </div>
         ))}
       </div>
-      <footer className="chat-footer">
-        <TextInput
-          value={newMessage}
-          placeholder="Type message here"
-          variant="transparent"
-          onChange={(e) => setNewMessage(e.target.value)}
-        />
-        <Button
-          className="send-button"
-          variant="transparent"
-          disabled={!newMessage}
-          onClick={() => {
-            sendMessage(newMessage);
-            setNewMessage('');
-          }}
-        >
-          Send
-        </Button>
-      </footer>
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        sendMessage(newMessage);
+        setNewMessage('');
+      }}>
+        <footer className="chat-footer">
+          <TextInput
+            value={newMessage}
+            placeholder="Type message here"
+            variant="transparent"
+            onChange={(e) => setNewMessage(e.target.value)}
+          />
+          <Button
+            className="send-button"
+            variant="transparent"
+            disabled={!newMessage}
+            type="submit"
+          >
+            Send
+          </Button>
+        </footer>
+      </form>
       <style jsx>{`
         .messages-container {
           flex: 1;
